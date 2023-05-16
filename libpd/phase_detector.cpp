faad9c8c0656c4847fbd5f0ff3deaa6ac24e3bf3
Patrick Lavin <plavin997@gmail.com>
2023-05-16 17:40:39 -0400

    chekpoint before cmake

--- libpd/phase_detector.cpp
+++ libpd/phase_detector.cpp
@@ -7,3 +7,12 @@ std::map<uint64_t, int64_t> run_pd(std::vector<uint64_t> addrs, double threshold
     PhaseDetector pd(threshold, interval_len, log2_signature_len, drop_bits);
     return pd.detect(addrs);
 }
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(FastPhaseDetector, m) {
+    m.doc() = "A module providing fast phase detection."; // optional module docstring
+
+    m.def("run_pd", &run_pd, "Run the phase detector on a vector of addresses",
+            py::arg("addrs"), py::arg("threshold"), py::arg("interval_len"), py::arg("log2_signature_len"), py::arg("drop_bits"));
+}
