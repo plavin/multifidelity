24b741ad8e1ba3bda9a9cf5bcfdb8de05d85676a
Patrick Lavin <plavin997@gmail.com>
2023-06-03 10:40:04 -0400

    begin work on optimizer

---
+++ optimize/main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// Boost includes
+#include <boost/filesystem.hpp>
+
+int main(int argc, char** argv) {
+
+    bool have_outfile = false;
+
+    if (argc < 2) {
+        std::cout << "Usage: ./optimize <trace-directory>" << std::endl;
+    }
+
+    if (
+
+}
