4870451f524ba4ab58574fd98ba65df11a995963
Patrick Lavin <plavin997@gmail.com>
2023-09-08 08:07:28 -0400

    subdirectory for spec stuff

--- CommonUses.md
+++
@@ -1,5 +0,0 @@
-# Some useful scripts
-
-## Collect traces
-
-./simulation.py -s two-level.py -c polybench-workloads.py -t 
---
+++ workloads/default-workload.py
@@ -0,0 +1,11 @@
+# ------------------------------------------------
+# This is a simple config file used for testing.
+# ------------------------------------------------
+# Notes:
+#  - If you use an input smaller that 15, then you
+#    may not see parallel behavior on 4 threads.
+
+{
+    'mm' : { 'cmd': 'mm 15  5', 'directory': './'},
+    'mm2' : { 'cmd': 'mm 20  5', 'directory': './'},
+ }
